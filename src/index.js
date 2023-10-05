8af7bc45bc1233dd3dae8511139d198d88781e26
OksanaBebesh <o.bebesh@itransition.com>
2023-10-05 18:58:35 +0200

    feat: check string on config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+let usedConfig = [];
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  return checkAll(str, bracketsConfig).length === 0;
+};
+
+const cleaningStr = (str, symbolToCut) => {
+  let strClean = "";
+  strClean = str.replace(symbolToCut, "");
+  return strClean;
+};
+
+const checkAll = (str, configRules) => {
+  let masksArray = [];
+  let finalStr = str;
+  configRules.forEach((rule) => {
+    let ruleCombine = rule.toString().replace(",", "");
+    masksArray.push(ruleCombine);
+    finalStr = cleaningStr(finalStr, ruleCombine);
+  });
+
+  if (finalStr.length === 0) return "";
+
+  masksArray.forEach((rule) => {
+    if (finalStr.includes(rule) === true && finalStr.length > 0) {
+      finalStr = cleaningStr(finalStr, rule);
+      finalStr = checkAll(finalStr, configRules);
+    }
+  });
+  return finalStr;
+};
